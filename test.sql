b7cffe345c00ce0609d5b31765972739e4dbc8bb
Jellaw <69714914+Jellaw@users.noreply.github.com>
2021-06-18 00:30:56 +0700

    Add files via upload

--- test.sql
+++ test.sql
@@ -8,10 +8,10 @@ Password varchar(255),
 PRIMARY KEY (ID)
 );
 CREATE TABLE employee (
-AccountID int(10) NOT NULL, 
 name varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-Age int(3) NOT NULL,
-Dob date NOT NULL, 
+accid int(3) NOT NULL,
+Dob varchar(20) NOT NULL, 
+gender varchar(20) not null,
 Address varchar(255) NOT NULL, 
 Phone varchar(255) NOT NULL, 
 position varchar(255) NOT NULL, 
@@ -24,11 +24,11 @@ ALTER TABLE employee
 ALTER TABLE employee
   MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
   
-ALTER TABLE Employee ADD CONSTRAINT FKEmployee390368 FOREIGN KEY (AccountID) REFERENCES Account (ID);
+/*ALTER TABLE Employee ADD CONSTRAINT FKEmployee390368 FOREIGN KEY (AccountID) REFERENCES Account (ID);*/
 insert into account(id,username,password) values
 ('1','vanb','123');
 
-INSERT INTO employee (id,name, age, dob, address, email,phone, position,salary,AccountID) VALUES
-('1','Nguyễn văn B', 22, '2017-11-15','Hà Nội' ,'vanb@gamil.com', '0917563254','Nhân viên','50000000','1');
+INSERT INTO employee (id,name, accid, dob,gender, address, email,phone, position,salary) VALUES
+('1','Nguyễn văn B', 1, '2017-11-15','Nam','Hà Nội' ,'vanb@gamil.com', '0917563254','Nhân viên','50000000');
 
 
